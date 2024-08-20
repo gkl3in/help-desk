c7663f0bab5f885e8a79222620c69c949dfee2c7
Gabriel Klein <gabrielklein289@hotmail.com>
2024-08-20 14:43:56 -0300

    feat: adicionado userRepository

---
+++ user-service-api/src/main/java/br/com/gklein/userserviceapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.gklein.userserviceapi.repository;
+
+import br.com.gklein.userserviceapi.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(final String email);
+
+    void deleteByEmail(final String validEmail);
+}

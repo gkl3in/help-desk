5770af0e8465fe8faa5c0d633bb66158cf1a3c50
Gabriel Klein <gabrielklein289@hotmail.com>
2024-10-20 15:29:31 -0300

    feat: add integration test to create new user controller with fields validations

--- user-service-api/src/test/java/br/com/gklein/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/br/com/gklein/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -5,6 +5,7 @@ import br.com.gklein.userserviceapi.entity.User;
 import br.com.gklein.userserviceapi.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.requests.CreateUserRequest;
+import models.requests.UpdateUserRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static br.com.gklein.userserviceapi.creator.CreatorUtils.generateMock;
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -122,6 +122,289 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.timestamp").isNotEmpty());
 
         userRepository.deleteById(entity.getId());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when name is empty")
+    void testSaveUserWithNameEmptyThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withName("").withEmail(VALID_EMAIL);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name must contain between 3 and 50 characters')]").exists())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when name is null")
+    void testSaveUserWithNameNullThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withName(null).withEmail(VALID_EMAIL);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when name is blank")
+    void testSaveUserWithNameContainingOnlySpacesThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withName("   ").withEmail(VALID_EMAIL);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when name has less than tree characters")
+    void testSaveUserWithNameContainingLessThenTreeCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withName("ab").withEmail(VALID_EMAIL);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name must contain between 3 and 50 characters')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when email is invalid")
+    void testSaveUserWithInvalidEmailThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Invalid email')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when email is empty")
+    void testSaveUserWithNullEmailThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withEmail(null);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Email cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when email has less than six characters")
+    void testSaveUserWithEmailContainingLessThenSixCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withEmail("a@b.c");
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Email must contain between 6 and 50 characters')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when password is empty")
+    void testSaveUserWithNullPasswordThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withEmail(VALID_EMAIL).withPassword(null);
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='password' && @.message=='Password cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when password has only empty spaces")
+    void testSaveUserWithPasswordContainingOnlySpacesThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withEmail(VALID_EMAIL).withPassword("   ");
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='password' && @.message=='Password must contain between 6 and 50 characters')]").exists())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='password' && @.message=='Password cannot be empty')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when password has less than six characters")
+    void testSaveUserWithPasswordContainingLessThenSixCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(CreateUserRequest.class).withEmail(VALID_EMAIL).withPassword("   ");
+
+        mockMvc.perform(
+                        post(BASE_URI)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='password' && @.message=='Password must contain between 6 and 50 characters')]").exists());
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when name is less than three characters")
+    void testUpdateUserWithNameLessThenThreeCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(UpdateUserRequest.class).withName("ab");
+        final var VALID_ID = userRepository.save(generateMock(User.class)).getId();
+
+        mockMvc.perform(
+                        put(UPDATE_URI, VALID_ID)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI + "/" + VALID_ID))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='name' && @.message=='Name must contain between 3 and 50 characters')]").exists());
+
+        userRepository.deleteById(VALID_ID);
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when email is less than six characters")
+    void testUpdateUserWithEmailLessThenSixCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(UpdateUserRequest.class).withEmail("a@b.c");
+        final var VALID_ID = userRepository.save(generateMock(User.class)).getId();
+
+        mockMvc.perform(
+                        put(UPDATE_URI, VALID_ID)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI + "/" + VALID_ID))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Email must contain between 6 and 50 characters')]").exists());
+
+        userRepository.deleteById(VALID_ID);
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when email is formatted incorrectly")
+    void testUpdateUserWithEmailFormattedIncorrectlyThenThrowBadRequest() throws Exception{
+        final var request = generateMock(UpdateUserRequest.class).withEmail("abc");
+        final var VALID_ID = userRepository.save(generateMock(User.class)).getId();
+
+        mockMvc.perform(
+                        put(UPDATE_URI, VALID_ID)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI + "/" + VALID_ID))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Invalid email')]").exists());
+
+        userRepository.deleteById(VALID_ID);
+    }
+
+    @Test
+    @DisplayName("Should throw a bad request exception when password has less than six characters")
+    void testUpdateUserWithPasswordLessThenSixCharactersThenThrowBadRequest() throws Exception{
+        final var request = generateMock(UpdateUserRequest.class).withPassword("12345").withEmail(VALID_EMAIL);
+        final var VALID_ID = userRepository.save(generateMock(User.class).withId(null)).getId();
+
+        mockMvc.perform(
+                        put(UPDATE_URI, VALID_ID)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(VALIDATION_ATTRIBUTES_ERROR_MSG))
+                .andExpect(jsonPath("$.error").value(VALIDATION_EXCEPTION_MSG))
+                .andExpect(jsonPath("$.path").value(BASE_URI + "/" + VALID_ID))
+                .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='password' && @.message=='Password must contain between 6 and 50 characters')]").exists());
+
+        userRepository.deleteById(VALID_ID);
+    }
+
+    @Test
+    @DisplayName("Should throw a not found exception when id is not found")
+    void testUpdateUserWithIdNotFoundThenThrowNotFoundException() throws Exception{
+        final var request = generateMock(UpdateUserRequest.class).withEmail(VALID_EMAIL);
+
+        mockMvc.perform(
+                        put(UPDATE_URI, 1L)
+                                .contentType(APPLICATION_JSON)
+                                .content(toJson(request))
+                ).andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Object not found. Id: 1, Type: UserResponse"))
+                .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value(BASE_URI + "/1"))
+                .andExpect(jsonPath("$.status").value(NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
     }
 
     private String toJson(final Object object) throws Exception {

9081bb972aafe6a4fa1c9296bc18046ccaf6f3b7
Gabriel Klein <gabrielklein289@hotmail.com>
2024-10-22 19:59:36 -0300

    feat: add jwt authentication impl

--- auth-service-api/src/main/java/br/com/gabrielklein/authserviceapi/controllers/impl/AuthControllerImpl.java
+++ auth-service-api/src/main/java/br/com/gabrielklein/authserviceapi/controllers/impl/AuthControllerImpl.java
@@ -1,22 +1,28 @@
 package br.com.gabrielklein.authserviceapi.controllers.impl;
 
 import br.com.gabrielklein.authserviceapi.controllers.AuthController;
+import br.com.gabrielklein.authserviceapi.security.JWTAuthenticationImpl;
+import br.com.gabrielklein.authserviceapi.utils.JWTUtils;
 import lombok.RequiredArgsConstructor;
 import models.requests.AuthenticateRequest;
 import models.responses.AuthenticationResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthControllerImpl implements AuthController {
 
+    private final JWTUtils jwtUtils;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
 
     @Override
-    public ResponseEntity<AuthenticationResponse> authenticate(AuthenticateRequest request) throws Exception {
-        return ResponseEntity.ok().body(AuthenticationResponse.builder()
-                        .type("Bearer")
-                        .token("token")
-                .build());
+    public ResponseEntity<AuthenticationResponse> authenticate(final AuthenticateRequest request) throws Exception {
+        return ResponseEntity.ok().body(
+                new JWTAuthenticationImpl(jwtUtils, authenticationConfiguration.getAuthenticationManager())
+                        .authenticate(request)
+        );
     }
 }

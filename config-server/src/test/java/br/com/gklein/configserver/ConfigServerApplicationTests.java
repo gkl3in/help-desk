f769f262a855a5a8c2b60a4895a517f22fb36028
Gabriel Klein <gabrielklein289@hotmail.com>
2024-10-27 15:14:21 -0300

    feat: create the config-server project

---
+++ config-server/src/test/java/br/com/gklein/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gklein.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

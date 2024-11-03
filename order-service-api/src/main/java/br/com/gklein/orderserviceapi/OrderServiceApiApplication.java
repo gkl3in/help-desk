eb2eece39a2f58622a13da1ba9c63127af7e2b1b
Gabriel Klein <gabrielklein289@hotmail.com>
2024-11-03 15:55:26 -0300

    feat: Create the order status enum on common lib.

--- order-service-api/src/main/java/br/com/gklein/orderserviceapi/OrderServiceApiApplication.java
+++ order-service-api/src/main/java/br/com/gklein/orderserviceapi/OrderServiceApiApplication.java
@@ -1,29 +1,12 @@
 package br.com.gklein.orderserviceapi;
 
-import br.com.gklein.orderserviceapi.entities.Order;
-import br.com.gklein.orderserviceapi.repositories.OrderRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class OrderServiceApiApplication implements CommandLineRunner {
-
-	private final OrderRepository repository;
+public class OrderServiceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		repository.save(Order.builder()
-				.requesterId("1")
-				.customerId("2")
-				.title("Order 1")
-				.description("Order 1 description")
-				.build());
 	}
 }

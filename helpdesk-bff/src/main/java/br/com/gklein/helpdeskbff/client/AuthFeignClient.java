8705e7abb1157657d5d1e369e29e638a46da8279
Gabriel Klein <gabrielklein289@hotmail.com>
2024-12-21 10:25:03 -0300

    feat: Changes on BFF for dev

--- helpdesk-bff/src/main/java/br/com/gklein/helpdeskbff/client/AuthFeignClient.java
+++ helpdesk-bff/src/main/java/br/com/gklein/helpdeskbff/client/AuthFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "auth-service-api",
+        name = "localhost:8765/auth-service-api", // TODO: Change this to the correct service name
         path = "/api/auth"
 )
 public interface AuthFeignClient {
--- helpdesk-bff/src/main/java/br/com/gklein/helpdeskbff/client/OrderFeignClient.java
+++ helpdesk-bff/src/main/java/br/com/gklein/helpdeskbff/client/OrderFeignClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @FeignClient(
-        name = "order-service-api",
+        name = "localhost:8765/order-service-api", // TODO: Change this to the correct service name
         path = "/api/orders"
 )
 public interface OrderFeignClient {
